fa3eaeb1247d3e16d2fc3f4c962a9110590a2d27
Guillaume Rochat <rochat.guillaume@gmail.com>
2018-11-12 22:16:37 -0500

    Add week parsing to see a full week.

--- cmd/week.go
+++ cmd/week.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -16,6 +17,37 @@ var weekCmd = &cobra.Command{
 	Long: `It does the sum of all the timestamps for the current week or displays 00:00 if there are no timestamp.
 The week beging on Sunday. Active timestamps (start without stop) are also taken into account.`,
 	Run: func(cmd *cobra.Command, argv []string) {
-		fmt.Println("week")
+		now := getOffsetNow()
+		begin := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
+		end := time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 99, time.UTC).AddDate(0, 0, 7)
+		duration, err := Read(begin.Format(time.RFC3339), end.Format(time.RFC3339))
+		if err != nil {
+			fmt.Println(fmt.Errorf("Could not read timestamps: %v", err))
+		}
+		fmt.Println(duration)
 	},
+}
+
+func getOffsetNow() time.Time {
+	now := time.Now()
+	offset := 0
+
+	switch now.Weekday() {
+	case time.Sunday:
+		offset = 0
+	case time.Monday:
+		offset = 1
+	case time.Tuesday:
+		offset = 2
+	case time.Wednesday:
+		offset = 3
+	case time.Thursday:
+		offset = 4
+	case time.Friday:
+		offset = 5
+	case time.Saturday:
+		offset = 6
+	}
+
+	return now.AddDate(0, 0, -offset)
 }
