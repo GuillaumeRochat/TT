25e9b3bc9a16c2b0e122033fd1546494d5b6b460
Guillaume Rochat <rochat.guillaume@gmail.com>
2018-11-12 22:06:00 -0500

    Add day parsing to see a single day.

--- cmd/day.go
+++ cmd/day.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -16,6 +17,13 @@ var dayCmd = &cobra.Command{
 	Long: `It does the sum of all the timestamp for the current day or displays 00:00 if there are no timestamp.
 Active timestamp (start without stop) are also taken into account.`,
 	Run: func(cmd *cobra.Command, argv []string) {
-		fmt.Println("day")
+		now := time.Now()
+		begin := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
+		end := time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 99, time.UTC)
+		duration, err := Read(begin.Format(time.RFC3339), end.Format(time.RFC3339))
+		if err != nil {
+			fmt.Println(fmt.Errorf("Could not read timestamps: %v", err))
+		}
+		fmt.Println(duration)
 	},
 }
--- cmd/store.go
+++ cmd/store.go
@@ -2,9 +2,11 @@ package cmd
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"os/user"
 	"path"
+	"time"
 
 	bolt "go.etcd.io/bbolt"
 )
@@ -65,19 +67,124 @@ func Write(bucketName BucketName, timestamp string) error {
 }
 
 // Read gets the timestamps of all buckets in the given range
-func Read(min string, max string) error {
+func Read(startDate string, stopDate string) (string, error) {
 	path, err := getPath()
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	db, err := bolt.Open(path, 0600, nil)
 	if err != nil {
-		return fmt.Errorf("Could not open database: %v", err)
+		return "", fmt.Errorf("Could not open database: %v", err)
 	}
 	defer db.Close()
 
-	// Do the read
+	minutes := 0.0
+	err = db.View(func(tx *bolt.Tx) error {
+		min := []byte(startDate)
+		max := []byte(stopDate)
 
-	return nil
+		starts := readStarts(tx, min, max)
+		stops := readStops(tx, min, max)
+
+		starts, stops, rangeErr := normalize(starts, stops)
+		if rangeErr != nil {
+			return rangeErr
+		}
+
+		diffs := getDiffs(starts, stops)
+		for _, diff := range diffs {
+			minutes += diff.Minutes()
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	duration, err := time.ParseDuration(fmt.Sprintf("%fm", minutes))
+	if err != nil {
+		return "", err
+	}
+
+	return formatDuration(duration), nil
+}
+
+func readStarts(tx *bolt.Tx, min []byte, max []byte) []time.Time {
+	tx.CreateBucketIfNotExists([]byte(Start))
+	startCursor := tx.Bucket([]byte(Start)).Cursor()
+	start := make([]time.Time, 0)
+
+	for key, value := startCursor.Seek(min); key != nil && bytes.Compare(key, max) <= 0; key, value = startCursor.Next() {
+		date, parseErr := time.Parse(time.RFC3339, string(value))
+		if parseErr == nil {
+			start = append(start, date)
+		}
+	}
+
+	return start
+}
+
+func readStops(tx *bolt.Tx, min []byte, max []byte) []time.Time {
+	tx.CreateBucketIfNotExists([]byte(Stop))
+	stopCursor := tx.Bucket([]byte(Stop)).Cursor()
+	stop := make([]time.Time, 0)
+
+	for key, value := stopCursor.Seek(min); key != nil && bytes.Compare(key, max) <= 0; key, value = stopCursor.Next() {
+		date, parseErr := time.Parse(time.RFC3339, string(value))
+		if parseErr == nil {
+			stop = append(stop, date)
+		}
+	}
+
+	return stop
+}
+
+func stopBeforeStart(start []time.Time, stop []time.Time) bool {
+	firstStart := start[0]
+	firstStop := stop[0]
+
+	return firstStop.Before(firstStart)
+}
+
+func normalize(starts []time.Time, stops []time.Time) ([]time.Time, []time.Time, error) {
+	if len(starts) > 0 && len(stops) > 0 {
+		if stopBeforeStart(starts, stops) {
+			starts = starts[1:]
+		}
+
+		if len(starts) != len(stops) {
+			if len(starts)-len(stops) == 1 {
+				stops = append(stops, time.Now())
+			} else {
+				return nil, nil, errors.New("Invalid range of start/stop")
+			}
+		}
+
+		return starts, stops, nil
+	}
+
+	return nil, nil, errors.New("Invalid range of start/stop")
+}
+
+func getDiffs(starts []time.Time, stops []time.Time) []time.Duration {
+	diffs := make([]time.Duration, 0)
+	for i := 0; i < len(starts); i++ {
+		start := starts[i]
+		stop := stops[i]
+
+		diffs = append(diffs, stop.Sub(start))
+	}
+
+	return diffs
+}
+
+func formatDuration(duration time.Duration) string {
+	duration = duration.Round(time.Minute)
+	hours := duration / time.Hour
+	duration -= hours * time.Hour
+	minutes := duration / time.Minute
+	return fmt.Sprintf("%02d:%02d", hours, minutes)
 }
